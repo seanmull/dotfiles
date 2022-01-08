86ba1ed311fde1dff114c1296250ec9bb147cfa0
seanmull <kimlan065@gmail.com>
2022-01-08 14:11:02 -0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+sudo apt update
+sudo apt upgrade
+sudo apt-get install curl
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+nvm install node
+sudo apt install cargo
+sudo apt install g++
+cargo install ripgrep
+pip install pyvim
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+sudo apt-get install zsh
+sudo apt install tmux
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt install neovim
+LV_BRANCH=rolling bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/rolling/utils/installer/install.sh)
