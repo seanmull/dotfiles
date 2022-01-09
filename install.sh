f01d0b97449643639c4e6cc5445545869f162563
seanmull <kimlan065@gmail.com>
2022-01-08 21:41:47 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,3 +27,21 @@ apt-get install x11-xserver-utils
 curl -fsSL https://fnm.vercel.app/install | bash
 curl -sS https://webinstall.dev/zoxide | bash
 sudo apt install alacritty
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt install tree
+sudo apt install unzip
+sudo apt install zip
+mkdir -p ~/.local/shae/fonts
+cd ~/.local/share/fonts && curl -fLo "Fira Code Regular Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.otf
