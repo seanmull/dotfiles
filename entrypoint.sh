b855d717c29a2ba0c5510bcb89e1f90378042d19
sean mull <kimlan065@gmail>
2023-09-06 04:33:55 +0000

    upate

---
+++ entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git config --global user.name "sean mull"
+
+git config --global user.email "kimlan065@gmail.com"
+
+ls -s /root/.ssh /home/lunaruser/.ssh
+
+bash /home/lunaruser/.local/bin/lvim
