546d5ef5eb1129d1e1cd5d1204dd07e3df481d94
Sean Mull <kimlan065@gmail.com>
2023-10-19 18:45:51 +1000

    changepath

--- lvim_docker.sh
+++ lvim_docker.sh
@@ -9,13 +9,12 @@ fi
 echo $lvim_path
 
 docker rm -f lvim && \
-docker run --rm -v $lvim_path:$lvim_path --net=host --ipc=host -e DISPLAY=:0 \
+docker run --rm -v $lvim_path:$lvim_path --net=host --ipc=host -e DISPLAY=$DISPLAY \
                                          -v lvim_share:/home/lunaruser/.local/share/lunarvim \
                                          -v lvim_config:/home/lunaruser/.config/lvim \
                                          --env="QT_X11_NO_MITSHM=1" \
                                          --env="_X11_NO_MITSHM=1" \
                                          -v /tmp/.X11-unix:/tmp/.X11-unix \
-                                         -v $XAUTHORITY:/tmp/.XAuthority -e XAUTHORITY=/tmp/.XAuthority \
                                          -v lvim_mason:/root \
                                          -v lvim_cache:/home/lunaruser/.cache/lvim \
                                          -v /home/s/vimwiki:/root/vimwiki \
--- lvim_exec.sh
+++ lvim_exec.sh
@@ -2,12 +2,11 @@
 docker rm -f lvim && \
   docker run --rm -d --name lvim \
   -v $(pwd):$(pwd) \
-  --net=host --ipc=host -e DISPLAY=:0 \
+  --net=host --ipc=host -e DISPLAY=$DISPLAY \
   --env="QT_X11_NO_MITSHM=1" \
   --env="_X11_NO_MITSHM=1" \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v lvim_share:/home/lunaruser/.local/share/lunarvim \
-  -v $XAUTHORITY:/tmp/.XAuthority -e XAUTHORITY=/tmp/.XAuthority \
   -v lvim_mason:/root \
   -v lvim_config:/home/lunaruser/.config/lvim \
   -v lvim_cache:/home/lunaruser/.cache/lvim \
