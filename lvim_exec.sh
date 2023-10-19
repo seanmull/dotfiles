d3452796bb84695f562e8caa8fea915a87add1ae
sean mull <kimlan065@gmail.com>
2023-10-19 10:49:44 +1000

    added capability for container to reach xserver

--- lvim_exec.sh
+++ lvim_exec.sh
@@ -2,7 +2,12 @@
 docker rm -f lvim && \
   docker run --rm -d --name lvim \
   -v $(pwd):$(pwd) \
+  --net=host --ipc=host -e DISPLAY=:0 \
+  --env="QT_X11_NO_MITSHM=1" \
+  --env="_X11_NO_MITSHM=1" \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v lvim_share:/home/lunaruser/.local/share/lunarvim \
+  -v $XAUTHORITY:/tmp/.XAuthority -e XAUTHORITY=/tmp/.XAuthority \
   -v lvim_mason:/root \
   -v lvim_config:/home/lunaruser/.config/lvim \
   -v lvim_cache:/home/lunaruser/.cache/lvim \
