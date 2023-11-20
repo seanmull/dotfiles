63fbfede42331ddcac5bc941bbb2167ebe869a9e
sean mull <kimlan065@gmail.com>
2023-11-20 15:35:50 +1000

    remove the user lib folder

--- lvim_docker.sh
+++ lvim_docker.sh
@@ -20,5 +20,4 @@ docker run --rm -v $home_path:$home_path --net=host --ipc=host -e DISPLAY=$DISPL
                                          -v lvim_cache:/home/lunaruser/.cache/lvim \
                                          -v /home/seanm@NIGHTLIFE.COM.AU/vimwiki:/root/vimwiki \
                                          -v /home/seanm@NIGHTLIFE.COM.AU/.ssh:/root/.ssh \
-                                         -v /usr/local/lib:/usr/local/lib \
                                          -w $lvim_path -it lvim
